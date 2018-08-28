0da965fa0639cb972c99199c14dcd708071aba4f
Wayne Collier <ke4tri@gmail.com>
2018-08-27 21:20:01 -0500

    added files

---
+++ objectchallenge.js
@@ -0,0 +1,49 @@
+let elizabethSanger = {
+    congressionalDistrict: 5,
+    platformStatment: [
+        {statement: "pie good", category: "Jobs"},
+        {statement: "Taxes good", category: "Taxes"},
+        {statement: "Roads good", category: "Infrastructure"}
+    ], 
+    donationFormUrl: 'www.okyou .com' ,
+    events: [
+        {date: '08/27/2018', title:"Waynes bday", description:"eat it all"},
+        {date: '08/09/2018', title:"Job day", description:"Party Really"},
+    ],
+    volunteers: [
+        {
+            name: "Cow boy",
+            Address: '1 main street',
+            email: 'kbgood@gmail.com',
+            phone: '1234211111',
+            availablity: 'all the time',
+            activities: 'soccer',
+        },
+        {
+            name: "Johnny Time",
+            Address: '6 main street',
+            email: 'sdfood@gmail.com',
+            phone: '12342345111',
+            availablity: 'never',
+            activities: 'j-Twon',
+
+        }
+        
+    ],
+    biography: "This is just going to be some text I am goin to write",
+    images: [
+        {
+            imageUrl: "www.jpg",
+            description: "best constitutne",
+            type: "constitution"
+        },
+        {
+            imageUrl: "time_time.jpg",
+            description: "When its time to leave",
+            type: "kat kat"
+        },
+      
+    ],
+    missionStatement: "Do good",
+    voterRegistrationUrl: 'www.blah.com'
+}
