f896b9bf24955f8d942d76c022deddc694933101
Wayne Collier <ke4tri@gmail.com>
2018-08-27 20:14:25 -0500

    corrected the replace 'newQuote2' to include spaces

--- stringchallenge.js
+++ stringchallenge.js
@@ -8,8 +8,8 @@ const printToDom = (stringToPrint, divId) => {
 const sonnet = "How like a winter hath my absence been From thee, the pleasure of the fleeting year! What freezings have I felt, what dark days seen! What old December's bareness everywhere! And yet this time removed was summer's time; The teeming autumn, big with rich increase, Bearing the wanton burden of the prime, Like widow'd wombs after their lords' decease: Yet this abundant issue seemed to me But hope of orphans, and unfathered fruit; For summer and his pleasures wait on thee, And, thou away, the very birds are mute: Or, if they sing, 'tis with so dull a cheer, That leaves look pale, dreading the winter's near.";
 
 const newQuote = sonnet.replace("winter", "yuletide");
-// const newQuote2 = sonnet.replace(/ the /g, " a large "); // use spaces makes sure it just 'the' and not words ith the in it
-const newQuote2 = sonnet.replace(/the/g, "a large"); // use spaces makes sure it just 'the' and not words ith the in it
+let newQuote2 = sonnet.replace(/ the /g, " a large "); // use spaces makes sure it just 'the' and not words ith the in it
+// const newQuote2 = sonnet.replace(/the/g, "a large"); // use spaces makes sure it just 'the' and not words ith the in it
 
 console.log(sonnet.indexOf('orphans'));
 console.log(sonnet.length);
